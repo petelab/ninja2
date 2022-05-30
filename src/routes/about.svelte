3bb4a6c2ae2747a548cf5c8897697bf14d4e536a
petelab <petelab@users.noreply.github.com>
2022-05-30 13:15:35 +0200

    init

---
+++ src/routes/about.svelte
@@ -0,0 +1,27 @@
+<script>
+</script>
+
+<div class="about">
+  <h2>About us</h2>
+  <p>
+    Lorem ipsum dolor sit amet consectetur adipisicing elit. Vitae pariatur
+    numquam, assumenda odit quis eius blanditiis, voluptas expedita, ducimus
+    minus corporis reprehenderit hic magni! Itaque cum quas mollitia molestias
+    qui optio ad? Nobis eos illum, corporis ad nesciunt cum porro voluptatibus
+    beatae, quaerat cumque excepturi pariatur atque quisquam quo officia, quasi
+    debitis facere unde voluptatem deleniti sint ex harum reprehenderit!
+  </p>
+  <a href="/guides">View Guides</a>
+  <a href="/">Home</a>
+</div>
+
+<style>
+  .about {
+    text-align: center;
+    display: block;
+    margin: 20px auto;
+  }
+  a {
+    display: block;
+  }
+</style>
---
+++ src/routes/index.svelte
@@ -0,0 +1,26 @@
+<script>
+  let title = "Ninja Gaming Guide";
+  const updateTitle = () => {
+    title = "different title";
+  };
+</script>
+
+<div class="index">
+  <h2>Welcome</h2>
+  <p>
+    Lorem, ipsum dolor sit amet consectetur adipisicing elit. Esse, repudiandae!
+  </p>
+  <a href="/guides">View Guides</a>
+  <a href="/about">About</a>
+</div>
+
+<style>
+  .index {
+    text-align: center;
+    display: block;
+    margin: 20px auto;
+  }
+  a {
+    display: block;
+  }
+</style>
